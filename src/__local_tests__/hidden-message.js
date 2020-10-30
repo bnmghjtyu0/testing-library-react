9052f41621347fbfe53dc6223dd301de9a04a906
RichardLiao <bnmghjtyu0@gmail.com>
2020-10-30 11:02:48 +0800

    [react-transition]: jest.mock('react-transition-group')

--- src/__local_tests__/hidden-message.js
+++ src/__local_tests__/hidden-message.js
@@ -2,11 +2,22 @@ import React from 'react'
 import {render, fireEvent, waitFor} from '@testing-library/react'
 import HiddenMessage from '../hidden-message'
 
-test('show hide message when toggle is clicked', () => {
-  const myMessage = 'hello world'
+jest.mock('react-transition-group', () => {
+  return {
+    CSSTransition: (props) => (props.in ? props.children : null),
+  }
+})
+
+test('show hide message when toggle is clicked', async () => {
+  const myMessage = `I'm a fade Transition!`
   const {getByText, queryByText, debug} = render(<HiddenMessage />)
   const toggleButton = getByText(/toggle/i)
   //   debug(toggleButton)
   expect(queryByText(myMessage)).not.toBeInTheDocument()
-  debug(toggleButton)
+  fireEvent.click(toggleButton)
+  expect(queryByText(myMessage)).toBeInTheDocument()
+  fireEvent.click(toggleButton)
+  expect(queryByText(myMessage)).not.toBeInTheDocument()
+  // 因為有用到 `react-transition-group`，所以這裡需要用 mock `jest.mock('react-transition-group'` ， 不能用 await 
+  // await waitFor(() => expect(queryByText(myMessage)).not.toBeInTheDocument())
 })
